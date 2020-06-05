17b1000efcf7688d70b3aaf983a678019cb9dfb2
“coreavparis2020m” <coreavparis2020m@gmail.com>
2020-06-05 10:00:54 +0200

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ListadoClientesComponent } from './clientes/listado-clientes/listado-clientes.component';
+import { CrearClienteComponent } from './clientes/crear-cliente/crear-cliente.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: InicioComponent},
+    {path: 'listado-clientes', component: ListadoClientesComponent},
+    {path: 'crear-cliente', component: CrearClienteComponent},
+    {path: '**', component: InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component';
+import { ListadoClientesComponent } from './clientes/listado-clientes/listado-clientes.component';
+import { CrearClienteComponent } from './clientes/crear-cliente/crear-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    BarraNavegacionComponent,
+    ListadoClientesComponent,
+    CrearClienteComponent
   ],
   imports: [
     BrowserModule,
