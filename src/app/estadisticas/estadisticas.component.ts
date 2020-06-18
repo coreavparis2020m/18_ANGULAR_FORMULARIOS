fb98e4f8177cf3ee30ad524b436d7ce20f54d614
“coreavparis2020m” <coreavparis2020m@gmail.com>
2020-06-18 13:04:28 +0200

    setting fechas

--- src/app/estadisticas/estadisticas.component.ts
+++ src/app/estadisticas/estadisticas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FacturasService } from '../servicios/facturas.service';
 
 @Component({
   selector: 'app-estadisticas',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticasComponent implements OnInit {
 
-  constructor() { }
+    facturas: any;
 
-  ngOnInit() {
-  }
+    primerDiaMesCurso: any;
+    ultimoDiaMesCurso: any;
+    primerDiaUltimoMes: any;
+    ultimoDiaUltimoMes: any;
+    primerDiaPenultimoMes: any;
+    ultimoDiaPenultimoMes: any;
+
+
+    constructor(private facturasService: FacturasService) { }
+
+    ngOnInit() {
+        this.loadFacturas();
+        this.setFechas();
+    }
+
+    loadFacturas() {
+        this.facturasService.getFacturas()
+                                .subscribe((res: any) => {
+                                    this.facturas = res.facturas;
+                                    console.log(this.facturas);
+                                }, (error: any) => {
+                                    console.log(error);
+                                })
+    }
+
+    setFechas() {
+        let mesCurso = new Date(2020, 1, 18).getMonth();
+        let ultimoMes = mesCurso - 1;
+        let penultimoMes = mesCurso - 2;
+        let anyoMesCurso = new Date(2020, 1, 18).getFullYear();
+        let anyoUltimoMes = anyoMesCurso;
+        let anyoPenultimoMes = anyoMesCurso;
+        if(mesCurso === 1) {
+            penultimoMes += 12;
+            anyoPenultimoMes -= 1;
+        } else if(mesCurso === 0) {
+            ultimoMes += 12;
+            penultimoMes +=12;
+            anyoUltimoMes -= 1;
+            anyoPenultimoMes -= 1;
+        }
+        //this.primerDiaMesCurso 
+        //this.ultimoDiaMesCurso
+        this.getDias(5, 2020);
+    }
+
+    getDias(mes, anyo) {
+        let dias = [];
+        let dia = new Date(anyo, mes, 1);
+        while(dia.getMonth() === mes) {
+            dias.push(new Date(dia));
+            dia.setDate(dia.getDate() + 1);
+        }
+        console.log(dias);
+    }
 
 }
